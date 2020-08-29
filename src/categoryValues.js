ed0faf8700cab0068a9f63d35e512442ce69e438
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-08-29 16:44:02 +0600

    Added array with all categories

---
+++ src/categoryValues.js
@@ -0,0 +1,14 @@
+const categoryValues = [
+    {name: 'Star Wars',
+    data: {title: 'Star Wars', id: 'star-wars'}},
+    {name: 'Famous People',
+    data: {title: 'Famous People', id: 'famous-people'}},
+    {name: 'Motivational',
+    data: {title: 'Motivational', id: 'motivational'}},
+    {name: 'Anime',
+    data: {title: 'Anime', id: 'anime'}},
+    {name: 'Humour',
+    data: {title: 'Humour', id: 'humour'}},
+]
+
+export default categoryValues;
